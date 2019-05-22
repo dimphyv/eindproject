142e85e5715c3edce053b876d5e24cb1a4fd7f99
LucSmeulders <luc@axito.com>
2019-05-22 22:07:13 +0200

    fix merge conflict

--- tinymvc/myapp/controllers/login.php
+++ tinymvc/myapp/controllers/login.php
@@ -4,8 +4,29 @@ session_start();
 
 class Login_Controller extends TinyMVC_Controller{
 
-	function checkUser(){
-		var_dump($_POST);
-		die();
+	function checkUser()
+	{
+		if($_SERVER['REQUEST_METHOD']=="POST")
+		{
+			if(isset($_POST['email']) AND isset($_POST['password']))
+			{
+				$email = $_POST['email'];
+				$password = $_POST['password'];
+				$this->load->model('user_model', 'users');
+				$pwMatch = $this->users->isPasswordMatch($email,$password);
+				if($pwMatch)
+				{
+					echo "goed, door naar het tonen van de evenementen";
+					//$_SESSION['status'] = array('OK','Email and password correct');
+					//relocator('welcome.php');
+					header('Location: /views/evenementen_view');
+				}
+				else {
+					echo "fout";
+					header('Location: /');
+				}
+			}
+		}
+		$this->view->display('index_view');
 	}
 }
